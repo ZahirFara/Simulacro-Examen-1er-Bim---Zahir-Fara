d03ede29705d1a21bd808acb8896a6587573b215
ZahirFara <167230564+ZahirFara@users.noreply.github.com>
2025-04-11 08:50:40 -0300

    Simulacro finalizado

---
+++ Assets/Scripts/gustos.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gustos : MonoBehaviour
+{
+    public string codigoHelado;
+    public float gramosHelado;
+    float precioGramo = 1.250f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (codigoHelado == "" || gramosHelado > 3000 || gramosHelado < 250)
+        {
+            Debug.Log("Los valores son validos");
+            return;
+        }
+        if (codigoHelado == "FRU")
+        {
+            Debug.Log("El precio sería " + gramosHelado * precioGramo * 0.9f);
+        }
+        else if (codigoHelado == "CHO")
+        {
+            Debug.Log("El precio sería " + gramosHelado * precioGramo);
+        }
+        else if (codigoHelado == "DDL")
+        {
+            Debug.Log("El precio sería " + gramosHelado * precioGramo);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
